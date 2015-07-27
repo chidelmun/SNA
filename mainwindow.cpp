ce697fd3501dca7d3916b98ae9278eec054be07b
Delveri Munang <chickdelveri@gmail.com>
2015-07-27 19:06:33 +0100

    Optimization of Progress Bar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,7 +10,7 @@ MainWindow::MainWindow(QWidget *parent) :
     QString safe= "QProgressBar::chunk {background: QLinearGradient( x1: 0, y1: 0, x2: 1, y2: 0,stop: 0 #78d,stop: 0.4999 #46a,stop: 0.5 #45a,stop: 1 #238 );border-bottom-right-radius: 7px;border-bottom-left-radius: 7px;border: 1px solid black;}";
      ui->progressBar->setStyleSheet(safe);
     ui->progressBar->reset();
-    this->setFixedSize(500,90);
+    this->setFixedSize(500,110);
 
 }
 
@@ -20,3 +20,13 @@ MainWindow::~MainWindow()
 }
 
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    for(int i=0;i<100;++i){
+        ui->progressBar->setValue(i);
+        system("sleep 0.01");
+    }
+
+    QMessageBox::information(this,"Complete", "Done");
+}
