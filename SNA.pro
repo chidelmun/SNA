af6b3de2ec4e85a4777258bac57ee5e8d8818f66
Delveri Munang <chickdelveri@gmail.com>
2015-07-28 08:54:38 +0100

    Enhancements on GUI

--- SNA.pro
+++ SNA.pro
@@ -13,8 +13,11 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp \
+    form.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  += mainwindow.h \
+    form.h
 
-FORMS    += mainwindow.ui
+FORMS    += mainwindow.ui \
+    form.ui
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,7 +10,8 @@ MainWindow::MainWindow(QWidget *parent) :
     QString safe= "QProgressBar::chunk {background: QLinearGradient( x1: 0, y1: 0, x2: 1, y2: 0,stop: 0 #78d,stop: 0.4999 #46a,stop: 0.5 #45a,stop: 1 #238 );border-bottom-right-radius: 7px;border-bottom-left-radius: 7px;border: 1px solid black;}";
      ui->progressBar->setStyleSheet(safe);
     ui->progressBar->reset();
-    this->setFixedSize(500,110);
+    this->setFixedSize(590,110);
+    ui->pushButton->setStyleSheet("background-color:rgb(166,120,222)");
 
 }
 
@@ -23,10 +24,25 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    for(int i=0;i<100;++i){
+    ui->label->setText("Starting.....");
+    for(int i=0;i<101;++i){
         ui->progressBar->setValue(i);
-        system("sleep 0.01");
-    }
+        system("sleep 0.05");
+        if(i==10){
+            ui->label->setText("Loading plugins.....");
+        }
+        else if(i==20){
+             ui->label->setText("Loading Databases.....");
+        }
 
-    QMessageBox::information(this,"Complete", "Done");
+        else if(i==50){
+             ui->label->setText("Loading Users.....");
+        }
+        else if(i==90){
+             ui->label->setText("Starting modules.....");
+        }
+    }
+    ui->label->setText("Complete");
+
+   // QMessageBox::information(this,"Complete", "Done");
 }
